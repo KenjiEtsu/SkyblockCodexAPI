6692ea99c7be609f783ecccac8e240fcf22f2982
Gerard Jimena Guerrero <68696641+KenjiEtsu@users.noreply.github.com>
2026-03-23 06:10:02 +0100

    Delete _check_election.mjs

--- _check_election.mjs
+++
@@ -1,8 +0,0 @@
-﻿(async () => {
-  const res = await fetch("https://api.hypixel.net/v2/resources/skyblock/election");
-  const data = await res.json();
-  console.log(Object.keys(data));
-  console.log("has election", !!data.election);
-  console.log("has current", !!data.current);
-  console.log("election candidates", data.election?.candidates?.length);
-})();
